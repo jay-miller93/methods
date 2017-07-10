eea268558b74db84093c38c9f8cf397cbee835b0
Jay <isaacsdaddy42293@gmail.com>
2017-07-10 09:50:01 -0400

    first commit

---
+++ methodPractice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //methods-do stuff, tend to belong to/interact with an object
+            //they end with (), and they come built in
+            //readline(),writeline(), split(), Reverse(), sort()
+
+
+        
+            
+
+        }
+    }
+}

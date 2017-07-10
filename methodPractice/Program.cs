ae7c8c68b299525e000b9936462407eff159f6b1
Jay <isaacsdaddy42293@gmail.com>
2017-07-10 10:27:53 -0400

    created first method app

--- methodPractice/Program.cs
+++ methodPractice/Program.cs
@@ -13,11 +13,39 @@ namespace methodPractice
             //methods-do stuff, tend to belong to/interact with an object
             //they end with (), and they come built in
             //readline(),writeline(), split(), Reverse(), sort()
+            //methods are a tool used for us to create reusable pieces of code
+            //methods are a way of us collecting a series of instructions..
+            //...and then call them when we need them
+            //methods are always part of a class.methods are always children classes
+            // a method will never be created inside of another method or member of the class
+            //methods are frequently called inside other class or method
+
+            Console.WriteLine("Hello, please enter the first number to be added.");
+            int numOne = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the second number to be added.");
+            int numTwo = int.Parse(Console.ReadLine());
 
 
-        
-            
+            int answer = Add(numOne, numTwo);
+            Console.WriteLine("The sum of your two numbers is " + answer);
+
+
+
 
         }
+        //method header-
+        //access modifier, return type, method name(in pascal Case)(first letter cap), parentheses
+        public static int Add(int firstNumber, int secondNumber)
+        {
+            //method body-
+            //ac complete method(header and body) is called method declaration
+
+            int sum = firstNumber + secondNumber;
+            return sum;
+            //the return takes whatever value is determined by using this method..
+            //..and sends that value back to wherever i call my method
+        }
+        //if you have a method that does NOT have a return value, you would use the keyword void
+        //when void we dont need to use return
     }
 }

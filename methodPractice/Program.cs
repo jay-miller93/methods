4fbb5b805f762575f8d8b9733fdeca75b834ec43
Jay <isaacsdaddy42293@gmail.com>
2017-07-10 12:03:29 -0400

    more practice

--- methodPractice/Program.cs
+++ methodPractice/Program.cs
@@ -25,6 +25,16 @@ namespace methodPractice
             //Console.WriteLine("Please enter the second number to be added.");
             //int numTwo = int.Parse(Console.ReadLine());
 
+            Food("Jay ", " pizza");
+            Food("Becky ", " ice cream");
+
+            RetCalc(24);
+
+            double hoursWorkedWeekly = 42.3d;
+            double hourlyWage = 12.50d;
+
+            Console.WriteLine("Your monthly wage is {0}.", WageCalculator(hoursWorkedWeekly, hourlyWage));
+
 
             //int answer = Add(numOne, numTwo);
             //Console.WriteLine("The sum of your two numbers is " + answer); 
@@ -32,51 +42,80 @@ namespace methodPractice
             //RobotWarning("Jay Miller");
             //Useless();
 
-            string myBirthMonth = "April";
-            string myFriendBirthMonth = "December";
-            string myVehicle = Vehicle(myBirthMonth);
-            string myFriendVehicle = Vehicle(myFriendBirthMonth);
-            Console.WriteLine("My future vehicle is {0} and Jordan's future vehicle is {1}.", myVehicle, myFriendVehicle);
+            //string myBirthMonth = "April";
+            //string myFriendBirthMonth = "December";
+            //string myVehicle = Vehicle(myBirthMonth);
+            //string myFriendVehicle = Vehicle(myFriendBirthMonth);
+            //Console.WriteLine("My future vehicle is {0} and Jordan's future vehicle is {1}.", myVehicle, myFriendVehicle);
 
 
-        }
-        //method header-
-        //access modifier, return type, method name(in pascal Case)(first letter cap), parentheses
-        public static int Add(int firstNumber, int secondNumber)
-        {
+            //}
+            ////method header-
+            ////access modifier, return type, method name(in pascal Case)(first letter cap), parentheses
+            ////public static int Add(int firstNumber, int secondNumber)
+            //{
             //method body-
             //ac complete method(header and body) is called method declaration
 
-            int sum = firstNumber + secondNumber;
-            return sum;
+            //int sum = firstNumber + secondNumber;
+            //return sum;
             //the return takes whatever value is determined by using this method..
             //..and sends that value back to wherever i call my method
         }
         //if you have a method that does NOT have a return value, you would use the keyword void
         //when void we dont need to use return
 
-        public static void RobotWarning(string name)
-        {
-            Console.WriteLine("Danger, " + name + "!!");
-        }
+        //public static void RobotWarning(string name)
+        //{
+        //    Console.WriteLine("Danger, " + name + "!!");
+        //}
 
-        public static void Useless()
-        {
-            Console.WriteLine("stuff");
-        }
+        //public static void Useless()
+        //{
+        //    Console.WriteLine("stuff");
+        //}
 
-        public static string Vehicle(string birthMonth)
+        //public static string Vehicle(string birthMonth)
+        //{
+        //    string vehicleFortune;
+        //    if(birthMonth.ToUpper() == "APRIL" || birthMonth.ToUpper() == "OCTOBER")
+        //    {
+        //        vehicleFortune = "Hoverboard";
+        //    }
+        //    else
+        //    {
+        //        vehicleFortune = "SUV";
+        //    }
+        //    return vehicleFortune;
+        //}
+        //create a method called favoritme food
+        //it should take two string parameters. one representing a name, the other representing a food
+        //the return type should be void
+
+        public static void Food(string name, string favFood)
         {
-            string vehicleFortune;
-            if(birthMonth.ToUpper() == "APRIL" || birthMonth.ToUpper() == "OCTOBER")
-            {
-                vehicleFortune = "Hoverboard";
-            }
-            else
-            {
-                vehicleFortune = "SUV";
-            }
-            return vehicleFortune;
+            Console.WriteLine(name + "likes" + favFood);
         }
+        ////Create a method called RetirementCalculator
+        //It should take an int as a parameter representing the user's age
+        //The method should calculate how many more years until the user retires using 65 as the age of retirement.
+        //The return type should be void
+        //Once it calculates the user's retirement age it should print
+        //"The user will retire in X years" where x represents the value that was calculated
+        public static void RetCalc(int userAge)
+        {
+            int retYrs = 65 - userAge;
+            Console.WriteLine("You will retire in " + retYrs + " years.");
+        }
+        //wage calculator
+        public static double WageCalculator(double hoursWorkedWeekly, double hourlyWage)
+        {
+            //this method should calculate the monthly wage
+            double monthlyWage = (hoursWorkedWeekly* 52 * hourlyWage) / 12;
+            return monthlyWage;
+        }
+        
+    
+       
     }
 }
